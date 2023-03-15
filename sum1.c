dcffeaac1d17e1f86897c8112a062ec38b49dfd2
Divanshu <124865944+Divanshu0320@users.noreply.github.com>
2023-03-15 21:48:25 +0530

    Add files via upload

---
+++ sum1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<math.h>
+
+int main() {
+    int a,b,c;
+    printf("enter a: ");
+    scanf("%d",&a);
+
+    printf("enter b: ");
+    scanf("%d",&b);
+
+    printf("enter c: ");
+    scanf("%d",&c);
+
+    printf("average is: %d",(a+b+c)/2);
+
+    return 0;
+}
